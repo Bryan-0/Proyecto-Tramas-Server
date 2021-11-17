9239f60791c399f30c53854e859c0196064ce8e8
Brayan Ayala <abryan172@gmail.com>
2021-11-16 23:07:11 -0600

    Prepare Prod

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='/')
 CORS(app)
 
 from app.apis import frame
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/tramas')
+def tramas():
+    return app.send_static_file('index.html')
